1791f335023d801b2cd8ab362015e619bbfe769a
philba <p.bayer@freemail.hu>
2016-09-25 11:10:51 +0200

    method for neighbour counting. bugfixes in areneighbour method

--- src/main/scala/at/baya/gol/GolEngine.scala
+++ src/main/scala/at/baya/gol/GolEngine.scala
@@ -9,12 +9,22 @@ class GolEngine {
 
 	type Point = (Long, Long)
 
-	private[gol] def areNeighbours(point1: Point, point2: Point) = {
+	private[gol] def numberOfNeighbours(point: Point, points: List[Point]): Long = {
+		points.count(areNeighbours(point, _))
+	}
+
+	private[gol] def areNeighbours(point1: Point, point2: Point): Boolean = {
+		if (point1.equals(point2)) {
+			return false
+		}
+
 		val x1 = point1._1
 		val y1 = point1._2
 		val x2 = point2._1
 		val y2 = point2._2
-		abs(x1 - x2) == 1 || abs(y1 - y2) == 1
+
+
+		abs(x1 - x2) <= 1 && abs(y1 - y2) <= 1
 	}
 
 }

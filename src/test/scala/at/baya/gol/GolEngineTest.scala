143a263b04e6bd43f20a0ba39695b7bfd72c8375
philba <p.bayer@freemail.hu>
2016-09-25 11:11:08 +0200

    testcases

--- src/test/scala/at/baya/gol/GolEngineTest.scala
+++ src/test/scala/at/baya/gol/GolEngineTest.scala
@@ -6,6 +6,8 @@ import org.scalatest.FunSpec
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.PropertyChecks
 
+import scala.math._
+
 /**
 	* Created by philba on 9/22/16.
 	*/
@@ -14,30 +16,50 @@ class GolEngineTest extends FunSpec with PropertyChecks {
 
 	val cut = new GolEngine
 
-	describe("Two points") {
+	describe("For the method areNeighbours") {
+		describe("two points") {
 
-		describe("are neighbours if their X and/or Y coordinates differ by exactly 1") {
+			describe("are neighbours if their X and/or Y coordinates differ by exactly 1") {
 
-			they("(50,50) and (51,51) should be neighbours") {
-				val result = cut.areNeighbours((50, 50), (51, 51))
-				assert(result == true)
-			}
-
-			they("(50,50) and (50,49) should be neighbours") {
-				val result = cut.areNeighbours((50, 50), (50, 49))
-				assert(result == true)
-			}
-
-			they("they should be neighbours for all valid points") {
-				val offsetGen = for {
-					xOffset <- Gen.oneOf(-1, 0, 1)
-					yOffset <- Gen.oneOf(-1, 0, 1)
-					if xOffset != 0 || yOffset != 0
-				} yield {
-					(xOffset, yOffset)
+				it("a point is not neighbour with itself") {
+					val result = cut.areNeighbours((50, 50), (50, 50))
+					assert(result == false)
 				}
 
-				val neighbourGen = for {
+				they("(50,50) and (51,51) should be neighbours") {
+					val result = cut.areNeighbours((50, 50), (51, 51))
+					assert(result == true)
+				}
+
+				they("(50,50) and (50,49) should be neighbours") {
+					val result = cut.areNeighbours((50, 50), (50, 49))
+					assert(result == true)
+				}
+
+				they("(50,50) and (51,50) should be neighbours") {
+					val result = cut.areNeighbours((50, 50), (50, 49))
+					assert(result == true)
+				}
+
+				they("(50,500) and (51,50) should be not neighbours") {
+					val result = cut.areNeighbours((50, 500), (50, 49))
+					assert(result == false)
+				}
+
+				they("(500,50) and (50,49) should not be neighbours") {
+					val result = cut.areNeighbours((500, 50), (50, 49))
+					assert(result == false)
+				}
+
+			}
+		}
+	}
+
+	describe("The method areNeighbours") {
+		describe("for all") {
+
+			def pointGen(offsetGen: Gen[(Int, Int)]) = {
+				for {
 					x1 <- Gen.choose[Long](0, 500)
 					y1 <- Gen.choose[Long](0, 500)
 					offsets <- offsetGen
@@ -46,14 +68,76 @@ class GolEngineTest extends FunSpec with PropertyChecks {
 					val cellNeighbour = (x1 + offsets._1, y1 + offsets._2)
 					(cell, cellNeighbour)
 				}
+			}
 
-				forAll(neighbourGen) {
-					(neighbours) => {
-						val result = cut.areNeighbours(neighbours._1, neighbours._2)
-						assert(result == true)
+			describe("invalid points") {
+				it("should return false") {
+					val nonNeighbourOffsetGen = for {
+						xOffset <- Gen.choose(-100, 100)
+						yOffset <- Gen.choose(-100, 100)
+						if abs(xOffset) > 1 || abs(yOffset) > 1
+					} yield {
+						(xOffset, yOffset)
+					}
+					val nonNeighbourGen = pointGen(nonNeighbourOffsetGen)
+
+					forAll(nonNeighbourGen) {
+						(neighbours) => {
+							val result = cut.areNeighbours(neighbours._1, neighbours._2)
+							assert(result == false)
+						}
 					}
 				}
 			}
+
+			describe("valid points") {
+				it("should return true") {
+					val neighbourOffsetGen = for {
+						xOffset <- Gen.oneOf(-1, 0, 1)
+						yOffset <- Gen.oneOf(-1, 0, 1)
+						if xOffset != 0 || yOffset != 0
+					} yield {
+						(xOffset, yOffset)
+					}
+					val neighbourGen = pointGen(neighbourOffsetGen)
+
+					forAll(neighbourGen) {
+						(neighbours) => {
+							val result = cut.areNeighbours(neighbours._1, neighbours._2)
+							assert(result == true)
+						}
+					}
+				}
+			}
+		}
+
+		describe("The method numberOfNeighbours") {
+			def pointList(points: Long*) = {
+				require(points.size % 2 == 0)
+				points
+					.sliding(2, 2)
+					.collect({ case Seq(a, b) => (a, b) })
+					.foldLeft(List.empty[(Long, Long)])((list, current) => current :: list)
+			}
+
+			it("should return 3 neighbours") {
+				println(pointList(50, 51, 49, 51, 51, 51, 52, 52, 48, 48, 48, 50, 52,
+					49))
+				println(pointList(50, 51, 49, 51, 51, 51, 52, 52, 48, 48, 48, 50, 52,
+					49).mkString)
+				println(pointList(50, 51, 49, 51, 51, 51, 52, 52, 48, 48, 48, 50, 52,
+					49).size)
+				val result = cut.numberOfNeighbours((50, 50), pointList(50, 51, 49, 51, 51, 51, 52, 52, 48, 48, 48, 50, 52,
+					49))
+				assert(result == 3)
+			}
+
+			it("should return 0 neighbours") {
+				val result = cut
+					.numberOfNeighbours((50, 50), pointList(52, 52, 48, 48, 48, 50, 52, 49, 100, 50, 51, 100, 51, 52))
+				assert(result == 0)
+			}
+
 		}
 
 	}

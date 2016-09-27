661f5ed15ef8c9891c09bb57eefb1c5f8ed939b9
philba <p.bayer@freemail.hu>
2016-09-27 22:13:10 +0200

    tests for drawing on canvas

--- src/test/scala/at/baya/gol/GolCanvasTest.scala
+++ src/test/scala/at/baya/gol/GolCanvasTest.scala
@@ -1,20 +1,27 @@
 package at.baya.gol
 
+import org.junit.runner.RunWith
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.FunSpec
 import org.scalatest.concurrent.Eventually
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, Matchers}
 
 import scalafx.beans.property.BooleanProperty
 
 /**
 	* Created by philba on 9/26/16.
 	*/
-class GolCanvasTest extends FunSpec with MockFactory with Eventually {
+@RunWith(classOf[JUnitRunner])
+class GolCanvasTest extends FunSpec with MockFactory with Eventually with Matchers {
+
+	def fixture = new {
+		val cut = new GolCanvas
+	}
 
 	describe("When the drawable property changes") {
 		val timesAddHandlerIsCalledDuringInitialization = 1
 
-		def fixture = new {
+		def mockFixture = new {
 			val addStub = stubFunction[Unit]("addStub")
 			val removeStub = stubFunction[Unit]("removeStub")
 			val cut = new GolCanvas {
@@ -25,7 +32,7 @@ class GolCanvasTest extends FunSpec with MockFactory with Eventually {
 		}
 
 		it("from the initial state to false, remove should be called once and add once") {
-			val f = fixture
+			val f = mockFixture
 			val newValue = BooleanProperty(false)
 			f.cut.drawable <== newValue
 
@@ -35,7 +42,7 @@ class GolCanvasTest extends FunSpec with MockFactory with Eventually {
 
 		it("from the initial state to false and back to true again, remove should be called once and add once for the " +
 			"initial change. For the second change only add should be called") {
-			val f = fixture
+			val f = mockFixture
 			val initialDrawable = BooleanProperty(false)
 			f.cut.drawable <== initialDrawable
 
@@ -46,8 +53,43 @@ class GolCanvasTest extends FunSpec with MockFactory with Eventually {
 				f.removeStub verify() never()
 				f.addStub verify() once()
 			}
-
 		}
+	}
+
+	describe("When the canvas is initialized") {
+		val cut = fixture.cut
+		it("should be empty") {
+			cut.getCells shouldBe empty
+		}
+
+		describe("When a cell is plotted on an empty canvas") {
+			val x = 2
+			val y = 3
+			val cut = fixture.cut
+
+			it("it should afterwards be the only cell on the canvas") {
+				cut.plotCell(x, y)
+				cut.getCells should contain only ((x, y))
+			}
+
+			describe("When the same cell is removed") {
+				it("the canvas should be empty again") {
+					cut.removeCell(x, y)
+					cut.getCells shouldBe empty
+				}
+			}
+		}
+
+		describe("When a number of cells are plotted on an empty canvas") {
+			val cut = fixture.cut
+			val cells = Seq[(Long, Long)]((1, 4), (55, 23), (43, 123))
+
+			they("should all be present on the canvas afterwards") {
+				cut.plotCells(cells)
+				cut.getCells should contain theSameElementsAs cells
+			}
+		}
+
 	}
 
 }
